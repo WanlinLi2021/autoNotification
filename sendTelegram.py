f14ad11824e6da0336a73d45f5c8f51e6cb4d77f
Wanlin Li <wanlinli2018@gmail.com>
2023-08-04 10:00:19 -0400

    get user

--- sendTelegram.py
+++ sendTelegram.py
@@ -1,52 +1,47 @@
-# Reference: https://www.youtube.com/watch?v=CNkiPN_WZfA
-# bot name: investors-dream
-# username: investorsDream_bot
-
-# Use version 13.13: pip install python-telegram-bot==13.13
-
-from telegram.ext import Updater, CommandHandler, MessageHandler
+# Reference: https://medium.com/codex/using-python-to-send-telegram-messages-in-3-simple-steps-419a8b5e5e2
+import requests
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
-TOKEN = os.getenv('teleToken')
+
+def get_userInfo(TOKEN):
+    # Getting your (client's) chat ID
+    #  This chat ID will be used in function ---> send_one_message(TOKEN, chat_id, message)
+    url = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
+    # print(requests.get(url).json())
+    return requests.get(url).json()
+    # Note: if you don’t send your Telegram bot a message, your results might be empty
+    # Q: How to get the user's Id ?
 
 
-def start(update, context):
-    update.message.reply_text("Hello! Welcome To Investors Dream Bot")
+def get_userId_text(TOKEN):
+    Id_Text = []
+    users_info = get_userInfo(TOKEN)
+    if users_info['ok']:
+        for item in range(len(users_info['result'])):
+            user_Id = users_info['result'][item]['message']['chat']['id']
+            user_text = users_info['result'][item]['message']['text']
+            # print(user_text)
+            Id_Text.append((user_Id, user_text))
+        return Id_Text
+    else:
+        print("No user information found")
 
 
-def help(update, context):
-    update.message.reply_text("""
-        The following commands are available:
-                              
-        /start -> Welcome Message
-        /help -> This Message
-        /about -> Information About Investors Dream                      
-""")
+# Sending your Telegram message using Python
+
+def send_one_message(TOKEN, chat_id, message):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
+    print(requests.get(url).json())  # this sends the message
 
 
-def about(update, context):
-    update.message.reply_text("""
-        Investor's Dream is a web application that provides users a multitude of investment tools, including
-        
-        /summary -> A summary of current investments held in multiple exchanges.
-        /portfolio -> A portfolio planner for analyzing and simulating theoretical portfolios.
-        /market-predictor -> A market predictor for viewing potential market movement.
-        /trading-strategies -> A set of trading strategies along with a tool for backtesting the strategies.
-        /buy-sell ->  An interface for buying or selling market assets through a connected exchange.
-""")
-
-
-updater = Updater(TOKEN)
-disp = updater.dispatcher
-
-# create a command handler, it's going to handle a certain command,
-# map a function to it and then add the command handler to the dispatcher
-disp.add_handler(CommandHandler("start", start))
-disp.add_handler(CommandHandler("help", help))
-disp.add_handler(CommandHandler("about", about))
-
-
-updater.start_polling()
-updater.idle()
+if __name__ == '__main__':
+    TOKEN = os.getenv('teleToken')
+    chat_id = os.getenv("test_teleID")
+    message = "hello from your telegram bot"
+    Id_Text = get_userId_text(TOKEN)
+    for (chat_id, message) in Id_Text:
+        print(chat_id)
+        print(message)
+        # send_one_message(TOKEN, chat_id, message)
